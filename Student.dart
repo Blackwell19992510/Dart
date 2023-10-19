a81fd5e5945ee37a7d8687a1529a26ab2eeccda6
Blackwell19992510 <mucciidayday@gmail.com>
2023-10-19 11:08:41 +0300

    dart class

---
+++ Student.dart
@@ -0,0 +1,121 @@
+class student {
+  String? Name;
+  int? Adminno;
+  String? Email;
+  String? Course;
+  int? PhoneNumber;
+  int? Age;
+  String? undergradUniName;
+  String? HighSchoolName;
+  student.degree_student(String n, int a, String e) {
+    this.Name = n;
+    this.Adminno = a;
+    this.Email = e;
+  }
+  student.dip_student(String n, int a, String e, int g, int p) {
+    this.Name = n;
+    this.Adminno = a;
+    this.Email = e;
+    this.Age = g;
+    this.PhoneNumber = p;
+  }
+
+  student.masters_student(
+      String n, int a, String e, int g, int p, String c, String h, String un) {
+    this.Name = n;
+    this.Adminno = a;
+    this.Email = e;
+    this.Age = g;
+    this.PhoneNumber = p;
+    this.Course = c;
+    this.HighSchoolName = h;
+    this.undergradUniName = un;
+  }
+
+  //Getter
+  //Setter-have a return type of void
+  String? getName() {
+    return Name;
+  }
+
+  int? getAdminno() {
+    return Adminno;
+  }
+
+  String? getEmail() {
+    return Email;
+  }
+
+  String? getCourse() {
+    return Course;
+  }
+
+  int? getPhoneNumber() {
+    return PhoneNumber;
+  }
+
+  int? getAge() {
+    return Age;
+  }
+
+  String? getUndergradUniName() {
+    return undergradUniName;
+  }
+
+  String? getHighSchoolName() {
+    return HighSchoolName;
+  }
+
+  //String? getName() => Name -Easier way to do it
+  void setName(String n) {
+    Name = n;
+  }
+
+  void setAdminno(int a) {
+    Adminno = a;
+  }
+
+  void setEmail(String e) {
+    Email = e;
+  }
+
+  void setCourse(String c) {
+    Course = c;
+  }
+
+  void setPhoneNumber(int p) {
+    PhoneNumber = p;
+  }
+
+  void setAge(int g) {
+    Age = g;
+  }
+
+  void setUnergradUniName(String un) {
+    undergradUniName = un;
+  }
+
+  void setHighSchoolName(String h) {
+    HighSchoolName = h;
+  }
+}
+
+void main() {
+  student degreeStudent =
+      new student.degree_student("Olivia", 003, "olivia@hotmail.com");
+  student diplomaStudent =
+      new student.dip_student("Muchi", 010, "muchi@gmail.com", 23, 0712345678);
+  student mastersStudent = new student.masters_student(
+      "Saul",
+      021,
+      "saul@gmail.com",
+      23,
+      0705260453,
+      "Computer Science",
+      "Chesamisi High",
+      "Daystar University");
+
+  print(degreeStudent.Name);
+  degreeStudent.setName("Victoria");
+  print(degreeStudent.Name);
+}
